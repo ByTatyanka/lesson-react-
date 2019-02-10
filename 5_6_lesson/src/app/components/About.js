f3993d99b90fe79771a2df976b38f663e7be824b
Valeriy S <ya.oven9010@yandex.ru>
2019-02-10 23:43:47 +0300

    урок 5-6: начало создания проекта(время: 1:16:38)

---
+++ 5_6_lesson/src/app/components/About.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class About extends Component {
+    render(){
+        return(
+            <div>
+                <h1>О нас</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias aliquam beatae culpa debitis deserunt et in minima molestias nihil nisi numquam optio saepe sapiente soluta, suscipit ullam, veritatis voluptas.</p>
+            </div>
+        )
+    }
+}
